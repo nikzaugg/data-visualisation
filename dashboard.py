d4453203d7426d07ac76dc5da9244e56ba98e89b
Nik Zaugg <nik.zaugg@bf.uzh.ch>
2017-10-07 10:40:26 +0200

    basic line plot functiona added

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 import csv
 import pandas as pd
 from bokeh.charts import Bar, output_file, show
+from bokeh.plotting import figure, output_file, show
 
 def loadDataSet_list():
     with open('bevgeburtenjahrgeschlquartstz.csv','r', encoding='utf8') as f:
@@ -22,5 +23,38 @@ def loadPlot1(year):
               tooltips = [('Area:', '@QuarLang'), ('Gender:', '@SexKurz'), ('Births:', '@height')])
     show(bar)
     output_file("dashboard.html")
+    
+def loadPlot2():
+    dataSet = pd.DataFrame( loadDataSet_pandas())
+    
+    dataSet_Female = dataSet[dataSet['SexKurz'] == 'W']
+    dataSet_Female = dataSet_Female.groupby(['StichtagDatJahr'])['AnzGebuWir'].sum().reset_index()
+    print(dataSet_Female)
+    
+    dataSet_Male = dataSet[dataSet['SexKurz'] == 'M']
+    dataSet_Male = dataSet_Male.groupby(['StichtagDatJahr'])['AnzGebuWir'].sum().reset_index()
+    print(dataSet_Male)
+    
+    dataSet_Total = dataSet
+    dataSet_Total = dataSet_Total.groupby(['StichtagDatJahr'])['AnzGebuWir'].sum().reset_index()
+    print(dataSet_Total)
+    
+    x = [1, 2, 3, 4, 5]
+    y = [6, 7, 2, 4, 5]
+    
+    # output to static HTML file
+    output_file("lines.html")
+    
+    # create a new plot with a title and axis labels
+    p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+    
+    # add a line renderer with legend and line thickness
+    p.line(dataSet_Female['StichtagDatJahr'], dataSet_Female['AnzGebuWir'], legend="females", line_width=2, line_color='blue')
+    p.line(dataSet_Male['StichtagDatJahr'], dataSet_Male['AnzGebuWir'], legend="males", line_width=2, line_color='red')
+    p.line(dataSet_Total['StichtagDatJahr'], dataSet_Total['AnzGebuWir'], legend="total", line_width=2, line_color='black')
+    
+    # show the results
+    show(p)
 
-loadPlot1(2015)
+    # loadPlot1(2015)
+loadPlot2()

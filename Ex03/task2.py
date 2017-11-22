c3fd77c875bb9fe5e3eea59968c9b36252fa1f30
Nik Zaugg <zauggnik@gmail.com>
2017-11-22 22:48:33 +0100

    task 2: added basic functions from slides

---
+++ Ex03/task2.py
@@ -0,0 +1,71 @@
+import csv
+import random
+import math
+import operator
+
+# Load dataset and split into trainingSet and testSet
+def loadDataset(filename, split, trainingSet=[] , testSet=[]):
+    with open(filename, 'rt', encoding='utf8') as csvfile:
+        lines = csv.reader(csvfile)
+        dataset = list(lines)
+        for x in range(len(dataset)-1):
+            for y in range(4):
+                dataset[x][y] = float(dataset[x][y])
+            if random.random() < split:
+                trainingSet.append(dataset[x])
+            else:
+                testSet.append(dataset[x])
+
+
+def euclideanDistance(instance1, instance2, length):
+    distance = 0
+    for x in range(length):
+        distance += pow((instance1[x] - instance2[x]), 2)
+    return math.sqrt(distance)
+
+
+def getNeighbors(trainingSet, testInstance, k):
+    distances = []
+    length = len(testInstance)-1
+    for x in range(len(trainingSet)):
+        dist = euclideanDistance(testInstance, trainingSet[x], length)
+        distances.append((trainingSet[x], dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbors = []
+    for x in range(k):
+        neighbors.append(distances[x][0])
+    return neighbors
+
+
+def getResponse(neighbors):
+    classVotes = {}
+    for x in range(len(neighbors)):
+        response = neighbors[x][-1]
+        if response in classVotes:
+            classVotes[response] += 1
+        else:
+            classVotes[response] = 1
+    sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedVotes[0][0]
+
+# Get the accuracy of the testset and predictions
+def getAccuracy(testSet, predictions):
+    correct = 0
+    for x in range(len(testSet)):
+        if testSet[x][-1] is predictions[x]:
+            correct += 1
+    return (correct/float(len(testSet)))*100.0
+
+
+####################################################
+# MAIN PROGRAM
+####################################################
+
+trainingSet=[]
+testSet=[]
+loadDataset('iris.data', 0.7, trainingSet, testSet)
+
+print(trainingSet)
+print('\n')
+print(testSet)
+

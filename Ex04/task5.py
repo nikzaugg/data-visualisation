535603da59feca79377778891afd3ae9fa03d216
Nik Zaugg <zauggnik@gmail.com>
2017-12-05 12:36:32 +0100

    task5: file added

---
+++ Ex04/task5.py
@@ -0,0 +1,17 @@
+# Data Visualisation
+# University of Zurich
+# Exercise 4
+# Nik Zaugg
+# 12-716-734
+# Task 5
+
+'''
+Select proper multivariate visualization techniques in order to present in a proper and informative
+way the relationship between multiple data variables. Create a grid of cells 10x10 on top of
+the map and display in each cell of the grid a vector-valued data attribute (e.g. wind direction
+and wind speed) using a directional marker (e.g. arrow with length for orientation and amplitude).
+Additionally, for 4 locations of your choice, select at least three scalar variables (e.g.
+temperature, pressure, precipitation) and present them below the map as scatterplot-matrices.
+You can chose the data values from the first hour of the hurricane simulation and using an elevation
+of 1km altitude.
+'''

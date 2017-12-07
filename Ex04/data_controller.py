221fbec28d3903240b796c734efde971391a8b53
Nik Zaugg <zauggnik@gmail.com>
2017-12-07 15:49:09 +0100

    documentation: added comments to data_controller.py

--- Ex04/data_controller.py
+++ Ex04/data_controller.py
@@ -1,7 +1,12 @@
+# Data Visualisation
+# University of Zurich
+# Exercise 4
+# Nik Zaugg
+# 12-716-734
+
 '''
 Interacts with the data sources inside /data
 '''
-
 
 import numpy as np
 import pandas as pd
@@ -9,14 +14,27 @@ import pandas as pd
 DATA_FOLDER = 'data/'
 
 def get_terrain_data():
+    '''
+    Load the terrain data of the dataset provided at http://vis.computer.org/vis2004contest/data.html
+    '''
     return np.memmap(DATA_FOLDER + 'HGTdata.bin', dtype='>f', shape=(500, 500))
 
 def read_data(datatype, hour):
-    # formating the hour input to string and format '##'
-    str_hour = str(hour)
+    '''
+    Load the corresponding variable of the dataset proved at http://vis.computer.org/vis2004contest/data.html
+
+    Arguments:
+    - datatype (string)
+    - hour (int)
+
+    Return:
+    - np.memmap: data is mapped to the memory and can be accessed without loading the whole dataset each time
+    '''
+
+    hour_as_string = str(hour)
     
     if hour < 10:
-        str_hour = '0' + str_hour
+        hour_as_string = '0' + hour_as_string
 
-    filename = DATA_FOLDER + datatype + 'f' + str_hour + '.bin'
-    return np.memmap(filename, dtype=">f", shape=(500, 500, 100), order='F')
+    file_name = DATA_FOLDER + datatype + 'f' + hour_as_string + '.bin'
+    return np.memmap(file_name, dtype=">f", shape=(500, 500, 100), order='F')
